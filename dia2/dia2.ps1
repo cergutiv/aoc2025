042cd003f62b2dcc53c6765a618aebdba6a8aafe
cergutiv <ivanjesuscere@gmail.com>
2025-12-02 12:14:11 +0100

    avanzado el dia 2 pero da resultados mu raros

--- dia2/dia2.ps1
+++ dia2/dia2.ps1
@@ -5,22 +5,32 @@ $valores = (Get-Content .\input).Split(',')
 foreach ($valor in $valores) {
     $ids = ($valor -split ',')
     foreach ($id in $ids) {
+        Write-Host $suma
         <# $id is the current item #>
         $delimitador = $id.IndexOf("-")
         # valor 1 del rango
         $min = $id.Substring(0, $delimitador)
         # ultimo valor del rango
         $max = $id.Substring($delimitador + 1)
+        $min = [int64]$min
+        $max = [int64]$max
         while ($min -lt $max) {
-            #comprobar si tiene valores que se repiten dentro de la string
-            ##############################################################################################################################
-            # AQUI ES DONDE ME LLEGO                                                                                                     #
-            ##############################################################################################################################
-            # i=0, mientras $i sea menor a la longitud de $min suma 1 a $i
-            
-
-            $min = $min + 1
+            $min = [string]$min
+            $array = @{}
+            foreach ($char in $min.ToCharArray()) {
+                if ($array.ContainsKey($char)) {
+                    $array[$char]++
+                }
+                else {
+                    $array[$char] = 1
+                }
+            }
+            if (($array.Values | Where-Object { $_ -gt 1 } | Measure-Object | Select-Object -ExpandProperty Count) -gt 0) {
+                $suma = $suma + $min
+            }
+            $min = [Int64]$min + 1
         }
     }
+        
 }
 Write-Host "la suma de los valores repetidos es:" $suma
