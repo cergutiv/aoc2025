358f2042ab9f369c86eabc8ce934e98485ac920c
cergutiv <ivanjesuscere@gmail.com>
2025-12-02 20:55:21 +0100

    hecho para que saque el rango y lo recorra en dia2

--- dia2/dia2.ps1
+++ dia2/dia2.ps1
@@ -1,11 +1,10 @@
 #variable para ir haciendo la suma
 $suma = 0
 #sacar los valores y separarlos de la coma
-$valores = (Get-Content .\input).Split(',')
+$valores = (Get-Content .\inputprueba).Split(',')
 foreach ($valor in $valores) {
     $ids = ($valor -split ',')
     foreach ($id in $ids) {
-        Write-Host $suma
         <# $id is the current item #>
         $delimitador = $id.IndexOf("-")
         # valor 1 del rango
@@ -14,23 +13,13 @@ foreach ($valor in $valores) {
         $max = $id.Substring($delimitador + 1)
         $min = [int64]$min
         $max = [int64]$max
-        while ($min -lt $max) {
-            $min = [string]$min
-            $array = @{}
-            foreach ($char in $min.ToCharArray()) {
-                if ($array.ContainsKey($char)) {
-                    $array[$char]++
-                }
-                else {
-                    $array[$char] = 1
-                }
-            }
-            if (($array.Values | Where-Object { $_ -gt 1 } | Measure-Object | Select-Object -ExpandProperty Count) -gt 0) {
-                $suma = $suma + $min
-            }
-            $min = [Int64]$min + 1
+        #recorremos el rango
+        for ($i = $min; $i -le $max; $i++) {
+            #convertimos a string
+            $string = $i.ToString()
+            $longitud = $string.Length
+            
         }
     }
         
 }
-Write-Host "la suma de los valores repetidos es:" $suma
